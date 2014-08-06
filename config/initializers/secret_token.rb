49b0e03076635777beb52e33116c8dd2edaf9868
Michael Patterson <mppatterson15@gmail.com>
2014-08-05 21:30:24 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SnailtrackerServer::Application.config.secret_key_base = '924c846046b2a936c64c4897763cef5f921a6e455baebae977603f13540ea437d6fa958c304e2e6a057a83ef3ef0e82276f1c5067021954c20adad11a079f708'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SnailtrackerServer::Application.config.session_store :cookie_store, key: '_snailtracker-server_session'
