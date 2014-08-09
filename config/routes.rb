01abf4949b21e65f608b87ceacde6221820c15fd
Michael Patterson <mppatterson15@gmail.com>
2014-08-08 21:37:09 -0500

    Organize routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 SnailtrackerServer::Application.routes.draw do
+  # API Resources
   resources :logs
-
   resources :notes
-
   resources :actions
-
   resources :action_types
-
   resources :sessions
 
+  # Web Resources
   resources :apps
 
   devise_for :users
