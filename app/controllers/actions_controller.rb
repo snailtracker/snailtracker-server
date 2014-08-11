054689cb8f6f200bebfda7c53d5c329905e28eaf
Michael Patterson <mppatterson15@gmail.com>
2014-08-10 21:48:10 -0500

    Allow CORS requests for API controllers.

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,2 +1,17 @@
 class ActionsController < ApplicationController
+  include Api
+
+  def create
+    action = Action.new(action_params)
+    action.session = @session
+    action.action_type = ActionType.find_by_name(params[:action][:action_type_name])
+    action.save
+    render_api_create_response action
+  end
+
+  private
+
+  def action_params
+    params.require(:action).permit(:url, :selector)
+  end
 end
--- app/controllers/concerns/api.rb
+++ app/controllers/concerns/api.rb
@@ -5,6 +5,9 @@ module Api
   included do
     before_filter :authenticate_api_key
     before_filter :set_session
+
+    before_filter :cors_preflight_check
+    after_filter :cors_set_access_control_headers
     protect_from_forgery :except => :create
   end
 
@@ -29,6 +32,20 @@ module Api
     api_key = params[:api_key]
     @app = App.find_by_api_key(api_key)
     return unauthenticated if api_key.nil? or @app.nil?
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == :options
+      cors_set_access_control_headers
+      render text: '', content_type: 'text/plain'
+    end
   end
 
   def unauthenticated
