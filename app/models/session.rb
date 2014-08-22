88192474a397e6f031c7a1de4009d095d7b638e1
Michael Patterson <mppatterson15@gmail.com>
2014-08-21 22:26:31 -0500

    Sessions.

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,16 @@
 class Session < ActiveRecord::Base
   include ApiKeyable
+  include ActionView::Helpers::DateHelper
   belongs_to :app
   has_many :actions
+
+  def description
+    "User ID: #{user_id || "unknown"} Name: #{user_name || "unknown"} Duration: #{duration}"
+  end
+
+  def duration
+    return if actions.count < 2
+    seconds = actions.last.updated_at - actions.first.updated_at
+    distance_of_time_in_words(seconds)
+  end
 end
