d4e2388bf0e3777d5f64bef90d7f8762f293f97d
Michael Patterson <mppatterson15@gmail.com>
2014-08-21 23:23:46 -0500

    Add change action type.

--- db/seeds/action_types.seeds.rb
+++ db/seeds/action_types.seeds.rb
@@ -3,3 +3,4 @@ ActionType.where({ name: "keyup" }).first_or_create
 ActionType.where({ name: "keypress" }).first_or_create
 ActionType.where({ name: "focus" }).first_or_create
 ActionType.where({ name: "blur" }).first_or_create
+ActionType.where({ name: "change" }).first_or_create
